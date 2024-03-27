376a0ab1f06c4c123888290caad8f242b63beb14
DavidLanders95 <davidlanders2009@hotmail.com>
2024-03-27 16:56:44 +0100

    Added more lens test and biprism interference test

--- src/temgymbasic/gui.py
+++ src/temgymbasic/gui.py
@@ -655,7 +655,7 @@ class SourceGUI(ComponentGUIWrapper):
         beam_tilt_y, beam_tilt_x = self.beam.tilt_yx
 
         self.rayslider, _ = labelled_slider(
-            num_rays, 1, 2**12, name="Number of Rays"
+            num_rays, 1, 2**18, name="Number of Rays"
         )
         self.rayslider.valueChanged.connect(self.try_update_slot)
 
