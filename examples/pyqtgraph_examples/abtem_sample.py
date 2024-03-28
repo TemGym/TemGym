8ad33fe7a0f8aae3b6a65558ac256bef32f1d80c
DavidLanders95 <davidlanders2009@hotmail.com>
2024-03-28 12:19:45 +0100

    Removing Phi_0 from rays, and duplicate code from source components

--- examples/pyqtgraph_examples/abtem_sample.py
+++ examples/pyqtgraph_examples/abtem_sample.py
@@ -5,29 +5,11 @@ from temgymbasic.model import (
 )
 from temgymbasic import components as comp
 
-
 import matplotlib.pyplot as plt
 import numpy as np
 from ase.build import graphene
-from typing import Tuple, NamedTuple
 import abtem
-from temgymbasic.plotting import plot_model
 from scipy.interpolate import RegularGridInterpolator as RGI
-
-class PlotParams(NamedTuple):
-    num_rays: int = 1024
-    ray_color: str = 'dimgray'
-    fill_color: str = 'aquamarine'
-    fill_color_pair: Tuple[str, str] = ('khaki', 'deepskyblue')
-    fill_alpha: float = 0.5
-    ray_alpha: float = 1.
-    component_lw: float = 1.
-    edge_lw: float = 1.
-    ray_lw: float = 0.1
-    label_fontsize: int = 12
-    figsize: Tuple[int, int] = (6, 6)
-    extent_scale: float = 1e-10
-
 
 atoms = abtem.orthogonalize_cell(graphene(vacuum=0.5))
 
@@ -39,7 +21,6 @@ potential = abtem.Potential(atoms, sampling=[0.05, 0.05])
 potential_array = potential.build().compute()
 print(potential_array.sampling, potential_array.gpts)
 pot_array = potential_array.array[0]
-extent = potential_array.extent*1e-10
 x = np.linspace(0., potential_array.extent[0], potential_array.gpts[0])*1e-10
 y = np.linspace(0., potential_array.extent[1], potential_array.gpts[1])*1e-10
 xx, yy = np.meshgrid(x, y)
