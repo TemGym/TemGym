3862a50c973215212cd9a4449e5ee78bd1cbc7b6
DavidLanders95 <davidlanders2009@hotmail.com>
2024-10-01 22:33:59 +0200

    Renamed gauss_gui_test to gauss_gui_example so pytest doesn't get confused. Added basic visual tests for aberrated lens - spherical aberration and coma

---
+++ examples/gaussian_beam_examples/gauss_gui_example.py
@@ -0,0 +1,61 @@
+from temgymbasic.model import (
+    Model,
+)
+from temgymbasic import components as comp
+import numpy as np
+from temgymbasic.utils import calculate_phi_0
+
+from PySide6.QtWidgets import QApplication
+from temgymbasic.gui import TemGymWindow
+import sys
+
+n_rays = 1000
+wavelength = 0.01
+
+size = 256
+det_shape = (size, size)
+pixel_size = 0.005
+
+wo = 0.1
+theta_x = 0
+theta_y = 0
+deg_yx = np.deg2rad((theta_y, theta_x))
+tilt_yx = np.tan(deg_yx)
+
+radius = 0.2
+lens_dist = 10
+focal = 0.7
+prop_dist = 50
+
+z_o = -lens_dist
+z_i = lens_dist + prop_dist
+m = 1e-11
+focal = prop_dist
+
+coeffs = [0, 0, 0, 0, 0]
+components = (
+    comp.ParallelBeam(
+        z=0.0,
+        voltage=calculate_phi_0(wavelength),
+        radius=radius,
+        #wo=wo,
+        tilt_yx=tilt_yx
+    ),
+    comp.AberratedLens(
+        z=lens_dist,
+        z1=-lens_dist,
+        z2=lens_dist + prop_dist,
+        f=focal,
+    ),
+    comp.Detector(
+        z=lens_dist + prop_dist,
+        pixel_size=pixel_size,
+        shape=det_shape,
+    ),
+)
+
+model = Model(components, backend='gpu')
+AppWindow = QApplication(sys.argv)
+viewer = TemGymWindow(model)
+viewer.show()
+AppWindow.exec()
