527db3f26f4b38cb3f439efdfe7616bd4b8ca074
Matthew Bryan <matthewbryan52@gmail.com>
2024-10-08 10:03:25 +0200

    Improve several parts of UI for TEM_example

--- examples/gaussian_beam_examples/TEM_example.py
+++ examples/gaussian_beam_examples/TEM_example.py
@@ -3,47 +3,38 @@ from temgymbasic.model import (
 )
 from temgymbasic import components as comp
 import numpy as np
-from temgymbasic.utils import calculate_phi_0
 
 from PySide6.QtWidgets import QApplication
 from temgymbasic.gui import TemGymWindow
 import sys
 
-n_rays = 1000
-wavelength = 0.01
+voltage = 100e3
 
 size = 256
 det_shape = (size, size)
-pixel_size = 0.005
+pixel_size = 10e-6
 
-wo = 0.1
 theta_x = 0
 theta_y = 0
 deg_yx = np.deg2rad((theta_y, theta_x))
 tilt_yx = np.tan(deg_yx)
 
-radius = 0.2
 lens_dist = 10
 prop_dist = 5
 
 z_o = -lens_dist
 z_i = lens_dist + prop_dist
-#m = 1e-11
-focal = 3
-
-f = (1 / z_i - 1 / z_o) **-1
-
-centre_yx = [0.0, 0.0]
+f = (1 / z_i - 1 / z_o) ** -1
 coeffs = [0., 0., 0., 0., 0.]
 
 components = (
     comp.GaussBeam(
         z=0.0,
-        voltage=calculate_phi_0(wavelength),
-        radius=0.0,
-        wo=wo,
+        voltage=voltage,
+        radius=50e-6,
+        wo=200e-6,
         tilt_yx=tilt_yx,
-        semi_angle = 0.01
+        semi_angle=0.0005,
     ),
     comp.DoubleDeflector(
         first=comp.Deflector(z=0.15, name='Upper OBJ Deflector'),
