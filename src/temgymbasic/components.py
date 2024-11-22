Tilt in radians, fix order, test case

src/temgymbasic/components.py
@@ -299,13 +299,22 @@ class DeflectingSample(STEMSample):
         self.scan_rotation = scan_rotation  # converts to radians in setter
         # [3, 2] - [[Δy, Δx], [ΔyΔy, ΔxΔy], [Δydx, ΔxΔx]]
         self.descan_error_shift = descan_error_shift
-        # [3, 2] - [[Δdy, Δdx], [ΔdyΔy, ΔdxΔy], [Δdydx, ΔdxΔx]]
+        # [3, 2] - [[Δrad-y, Δrad-x], [Δrad-yΔy, Δrad-xΔy], [Δrad-ydx, Δrad-xΔx]]
         self.descan_error_tilt = descan_error_tilt
+
+    @property
+    def descan_error_tilt_slope(self) -> np.ndarray:
+        return np.tan(self.descan_error_tilt)
 
     def step(
         self, rays: Rays
     ) -> Generator[Rays, None, None]:
         rays.location = self
+        if self.descan_error_tilt is not None:
+            # Adjusts existing tilts so inplace add onto dy/dx
+            slope_matrix = self.descan_error_tilt_slope
+            rays.dy += slope_matrix[0, 0] + rays.yx.T @ slope_matrix[1:, 0]
+            rays.dx += slope_matrix[0, 1] + rays.yx.T @ slope_matrix[1:, 1]
         if self.descan_error_shift is not None:
             # Neutral position is optical axis so replace totally
             rays.y = self.descan_error_shift[0, 0] + rays.yx.T @ self.descan_error_shift[1:, 0]
@@ -313,10 +322,6 @@ class DeflectingSample(STEMSample):
         else:
             rays.y[:] = 0
             rays.x[:] = 0
-        if self.descan_error_tilt is not None:
-            # Adjusts existing tilts so inplace add onto dy/dx
-            rays.dy += self.descan_error_tilt[0, 0] + rays.yx.T @ self.descan_error_tilt[1:, 0]
-            rays.dx += self.descan_error_tilt[0, 1] + rays.yx.T @ self.descan_error_tilt[1:, 1]
         yield rays
 
     @staticmethod

tests/test_components.py
@@ -582,20 +582,20 @@ def test_deflecting_sample_shift():
 
 
 def test_deflecting_sample_tilt():
-    # 1:1 descan tilt error to scan position
+    # 45 degree tilt error to scan position
     abstract_sample = comp.DeflectingSample(
         0., (8, 7),
         descan_error_tilt=np.asarray([
-            [0., 0.],   # [Δdy,   Δdx]
-            [1., 0.],   # [ΔdyΔy, ΔdxΔy]
-            [0., 1.],   # [Δdydx, ΔdxΔx]
+            [0.,              0.],   # [Δrad-y,   Δrad-x]
+            [np.pi / 4.,      0.],   # [Δrad-yΔy, Δrad-xΔy]
+            [0., -1 * np.pi / 4.]    # [Δrad-ydx, Δrad-xΔx]
         ]),
     )
     initial_rays = np.asarray([
         [1., 2., 3., 4.],  # x
         [0., 0., 0., 0.],  # dx
         [4., 3., 2., 1.],  # y
-        [0.1, 0.2, 0.3, 0.4],  # dy
+        [0., 0., 0., 0.],  # dy
         [0., 0., 0., 0.],  # _
     ])
     rays = Rays(
@@ -604,5 +604,5 @@ def test_deflecting_sample_tilt():
         path_length=0.,
     )
     after_rays = tuple(abstract_sample.step(rays))[0]
-    np.testing.assert_allclose(rays.x, after_rays.dx)
-    np.testing.assert_allclose(rays.y + rays.dy, after_rays.dy)
+    np.testing.assert_allclose(after_rays.dx, -1 * np.ones((rays.num,)) * initial_rays[0, :])
+    np.testing.assert_allclose(after_rays.dy, np.ones((rays.num,)) * initial_rays[2, :])